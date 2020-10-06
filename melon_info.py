3a2253867b280b792247a23bc1ef834e53b297f5
sunniraleigh <sraleigh@smith.edu>
2020-10-06 17:02:14 +0000

    adjust according to solution

--- melon_info.py
+++ melon_info.py
@@ -6,13 +6,10 @@ from melons import melons_info
 
 def print_melon(info_log):
     """Print each melon with corresponding attribute information."""
-    for melon in info_log:
-        print(f"{info_log[melon]}",
-        f"price: {info_log[melon][price]}",
-        f"seedless: {info_log[melon][seedless]}",
-        f"flesh_color: {info_log[melon][flesh_color]}",
-        f"weight: {info_log[melon][weight]}",
-        f"rind_color: {info_log[melon][rind_color]}"
-        )
+    for melon, attribute in info_log.items():
+        print(f"{info_log[melon]}")
+
+        for attribute, value in attribute.items():
+            print(f"{attribute}: {value}")
 
 print_melon(melons_info)
